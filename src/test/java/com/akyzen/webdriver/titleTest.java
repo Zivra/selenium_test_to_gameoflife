4daa8a289b284ad8f101bbc2a224faadf701fae5
Zivra <DevOps@zivra.com>
2015-11-11 15:23:00 -0700

    Update titleTest.java
    removing comments

--- src/test/java/com/akyzen/webdriver/titleTest.java
+++ src/test/java/com/akyzen/webdriver/titleTest.java
@@ -4,26 +4,9 @@
  * and open the template in the editor.
  */
 package com.akyzen.webdriver;
-/*
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 
 import junit.framework.Assert;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-*/
-import junit.framework.Assert;
-
-import org.junit.Test;
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 /**
@@ -34,16 +17,6 @@ public class titleTest {
     
     @Test
     public void titleNameTest() {
-        /*
-        WebDriver driver = new FirefoxDriver();
-        driver.navigate().to("http://52.32.248.199:8080/game-of-life/");
-
-        Assert.assertTrue("title should start differently", 
-                                                driver.getTitle().startsWith("The Game Of Life"));
-
-        driver.close();        
-                */
-        
         // Declaring and initialising the HtmlUnitWebDriver
         HtmlUnitDriver unitDriver = new HtmlUnitDriver();
         
@@ -54,27 +27,6 @@ public class titleTest {
                                                 unitDriver.getTitle().startsWith("The Game Of Life"));
 
         System.out.println("Title is: " + unitDriver.getTitle());
-        unitDriver.close();         
-
-        /*
-        // open google.com webpage
-        unitDriver.get("http://google.com");
-
-        System.out.println("Title of the page is 1-> " + unitDriver.getTitle());
-
-        // find the search edit box on the google page
-        WebElement searchBox = unitDriver.findElement(By.name("q"));
-
-        // type in Selenium
-        searchBox.sendKeys("Selenium");
-
-        // find the search button
-        //WebElement button = unitDriver.findElement(By.name("btnK"));//gbqfba
-
-        // Click the button
-        //button.click();
-
-        System.out.println("Title of the page is 2-> " + unitDriver.getTitle());
-        */
+        unitDriver.close();
     }
 }
