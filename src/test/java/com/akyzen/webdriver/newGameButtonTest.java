c1c003e7cb666d55594f7cf89c2d19f9735992c0
Zivra <DevOps@zivra.com>
2015-11-11 13:44:32 -0500

    Update newGameButtonTest.java
    Changed url of the unitDriver.navigate statement to use the new Test Instance URL - Ryan

--- src/test/java/com/akyzen/webdriver/newGameButtonTest.java
+++ src/test/java/com/akyzen/webdriver/newGameButtonTest.java
@@ -34,7 +34,7 @@ public class newGameButtonTest {
     public void newGameButtonClicTest() {
         //WebDriver driver = new FirefoxDriver();
         HtmlUnitDriver unitDriver = new HtmlUnitDriver();
-        unitDriver.navigate().to("http://52.33.95.230:8080/game-of-life/");
+        unitDriver.navigate().to("http://52.34.12.24:8080/demo/");
             
         WebElement newGameButton = unitDriver.findElement(By.linkText("New Game"));
         newGameButton.click();
