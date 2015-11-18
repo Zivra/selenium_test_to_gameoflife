6aac5a0bf2b8bc8bcc0e9f7866501574576e9f92
Zivra <DevOps@zivra.com>
2015-11-18 11:10:01 -0500

    Update titleTest.java
    Changed the IP Address of the Nexus Server - Ryan

--- src/test/java/com/akyzen/webdriver/titleTest.java
+++ src/test/java/com/akyzen/webdriver/titleTest.java
@@ -20,7 +20,7 @@ public class titleTest {
     public void titleNameTest() {
         // Declaring and initialising the HtmlUnitWebDriver
         HtmlUnitDriver unitDriver = new HtmlUnitDriver();
-        unitDriver.navigate().to("http://52.33.53.216:8080/game-of-life/");
+        unitDriver.navigate().to("http://52.34.130.26:8080/game-of-life/");
         
         Assert.assertTrue("title should start differently", 
                                                 unitDriver.getTitle().startsWith("The Game Of Life"));
