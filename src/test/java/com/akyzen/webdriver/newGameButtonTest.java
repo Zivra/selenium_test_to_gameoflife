eaf783b5fe23ba84eda9be6c41d0535fa96f86ea
Zivra <DevOps@zivra.com>
2016-02-17 21:42:54 -0500

    Changed IP address

--- src/test/java/com/akyzen/webdriver/newGameButtonTest.java
+++ src/test/java/com/akyzen/webdriver/newGameButtonTest.java
@@ -34,7 +34,7 @@ public class newGameButtonTest {
     public void newGameButtonClicTest() {
         //WebDriver driver = new FirefoxDriver();
         HtmlUnitDriver unitDriver = new HtmlUnitDriver();
-        unitDriver.navigate().to("http://52.32.53.249:8080/game-of-life");
+        unitDriver.navigate().to("http://52.36.139.14:8080/game-of-life");
             
         WebElement newGameButton = unitDriver.findElement(By.linkText("New Game"));
         newGameButton.click();
