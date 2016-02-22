80c7dbfdbe75b7e79fe70ab5f7e352148c82214f
Zivra <DevOps@zivra.com>
2016-02-22 16:06:17 -0500

    changed ip of test instance

--- src/test/java/com/akyzen/webdriver/titleTest.java
+++ src/test/java/com/akyzen/webdriver/titleTest.java
@@ -20,7 +20,7 @@ public class titleTest {
     public void titleNameTest() {
         // Declaring and initialising the HtmlUnitWebDriver
         HtmlUnitDriver unitDriver = new HtmlUnitDriver();
-        unitDriver.navigate().to("http://52.36.139.14:8080/game-of-life/");
+        unitDriver.navigate().to("http://52.33.196.64:8080/game-of-life/");
         
         Assert.assertTrue("title should start differently", 
                                                 unitDriver.getTitle().startsWith("The Game Of Life"));
